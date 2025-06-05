9cc3266b85488b87ae3a2cf77ad476f26a91183f
santiagosuarez32 <dlsantii86@gmail.com>
2025-06-05 11:45:09 -0300

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 
 import { Routes, Route } from "react-router-dom";
-import Ecommerce from "./components/Ecommerce";
 
-import AboutUs from "./components/about/AboutUs";
-import ContactForm from "./components/ContactForm";
 
 function App() {
 
